05b3242f7b003d447a758f37957647f42f5c8e49
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 17:45:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
-def my_hash_creator(key, value)
-  {:key => "value"}  # return a hash that includes the key and value parameters passed into this method
+def my_hash_creator(:name, "George")
+  {:name => "George"}  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
