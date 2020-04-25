413205eb80631050249db883a9e3df222680037f
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 17:38:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,5 @@
-def my_hash_creator(:name , "Grass Hopper")
-  {:name => "Grass Hooper"}
-
-  # return a hash that includes the key and value parameters passed into this method
+def my_hash_creator(key, value)
+  {:key => "value"}  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
